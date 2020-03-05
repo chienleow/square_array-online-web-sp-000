98d1d7a237b5d208bc12e6438c6f724458f526b9
C. Leow <chienleow1@gmail.com>
2020-03-05 03:22:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each { |i| new_array << i ** 2 }
   new_array
+=======
+  array.each
+  square_array(array)
+>>>>>>> 5cba610628770c2161d612ef08435c1dd6efe77e
 end
